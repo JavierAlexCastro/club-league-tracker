a91de4bd8c17750938d5df6e2d01ee9cfb47a6e4
JavierCastro <javieralexcastro95@gmail.com>
2022-10-02 18:33:24 -0500

    log request

--- club_league_tracker/networking/utils.py
+++ club_league_tracker/networking/utils.py
@@ -1,4 +1,6 @@
 import time
+import logging
+import sys
 
 from dataclasses import dataclass
 from enum import Enum
@@ -28,6 +30,7 @@ def do_retryable_request(request_type: RequestType,
                         request_contents: RequestContents,
                         retry_options: RetryOptions):
     query_response = None
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     for i in range(retry_options.max_retries):
         if query_response:
             break
@@ -61,6 +64,8 @@ def do_retryable_request(request_type: RequestType,
                                                 headers=request_contents.headers,
                                                 timeout=request_contents.timeout_seconds,
                                                 proxies=request_contents.proxy)
+        print(f"Request headers: {query_response.request.headers}")
+        print(f"Request body: {query_response.request.body}")
         print(f"Request response: {query_response}")
 
     if not query_response:
