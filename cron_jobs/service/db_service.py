ed21fb2e2e63c9d87edf134c43791c2d34417471
JavierCastro <javieralexcastro95@gmail.com>
2022-12-10 22:13:52 -0600

    use generator for no_longer_members, new_members and continuing_members

--- cron_jobs/service/db_service.py
+++ cron_jobs/service/db_service.py
@@ -31,17 +31,17 @@ def upsert_club_members(club_members: typing.List[ClubMember]):
     db_members_tags = [member.tag for member in db_members]
     req_members_tags = [member.tag for member in club_members]
 
-    no_longer_members = [i for i in db_members if not i.club_tag in req_members_tags]
-    new_members = [i for i in club_members if not i.club_tag in db_members_tags]
+    no_longer_members = (i for i in db_members if i.club_tag not in req_members_tags)
+    new_members = (i for i in club_members if i.club_tag not in db_members_tags)
     # continuing_members = [i for i in club_members if i.club_tag in db_members_tags]
-    continuing_members = [i for i in db_members if i.club_tag in req_members_tags]
+    continuing_members = (i for i in db_members if i.club_tag in req_members_tags)
 
-    print(f"db member tags: {len(db_members_tags)}, {db_members_tags}")
+    print(f"db member tags: {len(db_members_tags)}, {db_members_tags.tags}")
     print(f"req member tags: {len(req_members_tags)}, {req_members_tags}")
 
-    print(f"No longer members: {len(no_longer_members)}, {no_longer_members}")
-    print(f"New members: {len(new_members)}, {new_members}")
-    print(f"Continuing members: {len(continuing_members)}, {new_members}")
+    print(f"No longer members: {len(no_longer_members)}")
+    print(f"New members: {len(new_members)}")
+    print(f"Continuing members: {len(continuing_members)}")
 
     try:
         for member in new_members:
