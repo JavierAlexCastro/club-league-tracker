43664ae1a8fec5bfb72f7c85cff21b605bc8020d
JavierCastro <javieralexcastro95@gmail.com>
2022-12-12 01:26:32 -0600

    add a bit more verbose logging around db_member_details

--- cron_jobs/service/db_service.py
+++ cron_jobs/service/db_service.py
@@ -81,6 +81,7 @@ def upsert_club_members(club_members: typing.List[ClubMember], auth_token: str):
 
             member_details = get_member_details_from_api(member.tag, auth_token)
             db_member_details = db_get_club_member_details(member.tag)
+            print(f"For {member.tag} got details: {db_member_details}")
             member_details.update(db_session, member.tag, db_member_details.start_date, None, member_details.victories_trios, 
                                     member_details.victories_duos, member_details.victories_solo)
             updates+=1
@@ -90,6 +91,7 @@ def upsert_club_members(club_members: typing.List[ClubMember], auth_token: str):
 
             member_details = get_member_details_from_api(member.tag, auth_token)
             db_member_details = db_get_club_member_details(member.tag)
+            print(f"For {member.tag} got details: {db_member_details}")
             member_details.update(db_session, member.tag, db_member_details.start_date, func.now(), member_details.victories_trios, 
                                     member_details.victories_duos, member_details.victories_solo)
             deletions+=1
