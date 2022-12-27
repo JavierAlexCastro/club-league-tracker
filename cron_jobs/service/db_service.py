aa5d9d6ad0fd2f7087ce37408ce3355b515c76e6
JavierCastro <javieralexcastro95@gmail.com>
2022-12-26 23:36:41 -0600

    try adding to session before commiting

--- cron_jobs/service/db_service.py
+++ cron_jobs/service/db_service.py
@@ -34,7 +34,7 @@ def deprecate_club_league_season(season: ClubLeagueSeason):
     try:
         print(f"Deprecating season {season.week}")
         setattr(season, 'season_is_current', False)
-        # db_session.add(season)
+        db_session.add(season)
         db_session.commit()
         print(f"Deprecated season {season.week}")
     except Exception as ex:
