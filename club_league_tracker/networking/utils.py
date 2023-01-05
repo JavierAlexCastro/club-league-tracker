01a4abb96abd1e48812625bf7ee88bdf1ec80cf6
JavierCastro <javieralexcastro95@gmail.com>
2023-01-05 16:19:08 -0600

    tolerate 404 and log warnings

--- club_league_tracker/networking/utils.py
+++ club_league_tracker/networking/utils.py
@@ -64,6 +64,9 @@ def do_retryable_request(request_type: RequestType,
         print(f"Request response: {query_response}")
 
     if not query_response:
-        raise RuntimeError(f"Max retries exceeded sending {request_type} to {request_contents.url}. Got {query_response.text}")
+        if query_response.status_code is 404:
+            print(f"Warning! Got 404 sending {request_type} to {request_contents.url}. Message: {query_response.text}")
+        else:
+            raise RuntimeError(f"Max retries exceeded sending {request_type} to {request_contents.url}. Got {query_response.text}")
 
     return query_response
